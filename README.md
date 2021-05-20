c2e66074059aef0e1acaec34a1ef3d933bb2c12c
Bastian <wittmann_@gmx.de>
2021-05-20 16:53:10 +0200

    Moved open tasks/TODOs to README.md

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 The overall aim is to improve the results for the visual grounding task.
 
+## Open Tasks
+
+- Add argparse arguments for detector.
+- Change dimensions of features/querry from [B, 288, 256] to [B, 128, 256]
+  is possible via a MLP. DONE
+- Think about using a pre-trained detector. Maybe for first step pre-trained.
+  VoteNet vs. pre-trained detector. Reimplement load_state_dict in eval.py.
+- Make train.py work.
+- Think if it makes difference that transformer backbone was trained with xyz only.
+- Check if objectness mask gets determinated correctly. 
+  -> Should be correct, transformer head predicts objectness score (B, 256, 1) instead of (B, 256, 2)
+- Check shapes of added elements from loss_helper_detector and detector. DONE 
+- Check equivalence to vote_loss in loss_helper_detector.
+- Objectness_mask in loss only ones? 
+- Change end_points to data_dict globally.
+- Implement use_lang_classifier in loss_helper_detector.py.
+- Change coefficients in final loss in loss_helper_detector.py.
+- Add pretrained transformer weights in eval.py.
+
 ## References
 
 ### ScanRefer: 3D Object Localization in RGB-D Scans using Natural Language
--- models/refnetV2.py
+++ models/refnetV2.py
@@ -13,25 +13,6 @@ from models.match_module import MatchModule
 from models.detector import GroupFreeDetector
 
 
-# TODO: 
-# Add argparse arguments for detector.
-# Change dimensions of features/querry from [B, 288, 256] to [B, 128, 256]
-#   is possible via a MLP. DONE
-# Think about using a pre-trained detector. Maybe for first step pre-trained.
-#   VoteNet vs. pre-trained detector. Reimplement load_state_dict in eval.py.
-# Make train.py work.
-# Think if it makes difference that transformer backbone was trained with xyz only.
-# Check if objectness mask gets determinated correctly. 
-#   -> Should be correct, transformer head predicts objectness score (B, 256, 1) instead of (B, 256, 2)
-# Check shapes of added elements from loss_helper_detector and detector. DONE 
-# Check equivalence to vote_loss in loss_helper_detector.
-# Objectness_mask in loss only ones? 
-# Change end_points to data_dict globally.
-# Implement use_lang_classifier in loss_helper_detector.py.
-# Change coefficients in final loss in loss_helper_detector.py.
-# Add pretrained transformer weights in eval.py.
-
-
 class RefNetV2(nn.Module):
     def __init__(self, num_class, num_heading_bin, num_size_cluster, mean_size_arr, 
     input_feature_dim=0, num_proposal=128, vote_factor=1, sampling="vote_fps",
