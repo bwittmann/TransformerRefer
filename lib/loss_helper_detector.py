4e8115fa9e8560d4e36285ae436583874fdeb0db
isefos <philofoth@gmail.com>
2021-06-12 14:04:38 +0200

    improved masking out, now labels are also masked

--- lib/loss_helper_detector.py
+++ lib/loss_helper_detector.py
@@ -371,14 +371,17 @@ def compute_reference_loss(data_dict, config, use_multi_ref_gt=False):
         if use_multi_ref_gt:
             # set all predicted bboxes with iou over threshold as gt for the ref box
             labels[i, ious > MULTI_REF_IOU_THRESHOLD] = 1
-            num_gt = sum(labels[i, :] * objectness_masks[i, :])
-            num_total = sum(objectness_masks[i, :])
+            # mask out with objectness, since where no objectness, it won't be able to distinguish anyway -> were set to zero
+            labels[i, objectness_masks[i, :] == 0] = 0
+            num_gt = labels[i, :].sum()
+            num_total = objectness_masks[i, :].sum()
             # weigh the ref scores according to their ious and scale by inbalance,
             # the negatives -> if close to iou threshold almost 0 weight, if 0 iou 1 weight
-            weights[i, ious < MULTI_REF_IOU_THRESHOLD] = (num_total / (num_total - num_gt + 1e-8)) * (1 - (ious[ious < MULTI_REF_IOU_THRESHOLD] / MULTI_REF_IOU_THRESHOLD))
-            weights[i, ious > MULTI_REF_IOU_THRESHOLD] = (1 / ious[max_idx]) * (num_total / (num_gt + 1e-8)) * ious[ious > MULTI_REF_IOU_THRESHOLD]
-            # mask out with objectness, since where no objectness, it won't be able to distinguish anyway -> were set to zero
-            weights[i, :][objectness_masks[i] == 0] = 0
+            neg_scale = (num_total / (num_total - num_gt + 1e-8))
+            pos_scale = (num_total / (num_gt + 1e-8))
+            weights[i, ious < MULTI_REF_IOU_THRESHOLD] = neg_scale * (1 - (ious[ious < MULTI_REF_IOU_THRESHOLD] / MULTI_REF_IOU_THRESHOLD))
+            weights[i, ious > MULTI_REF_IOU_THRESHOLD] = pos_scale * (1 / ious[max_idx]) * ious[ious > MULTI_REF_IOU_THRESHOLD]
+            weights[i, objectness_masks[i, :] == 0] = neg_scale * 0.2
         else:
             # treat the bbox with highest iou score as the gt
             labels[i, max_idx] = 1
@@ -389,7 +392,7 @@ def compute_reference_loss(data_dict, config, use_multi_ref_gt=False):
     criterion = SoftmaxRankingLoss()
     if use_multi_ref_gt:
         # multiply by X to get comparable magnitude
-        loss_weights = torch.FloatTensor(15 * weights).cuda()
+        loss_weights = torch.FloatTensor(10 * weights).cuda()
         criterion = nn.BCEWithLogitsLoss(weight=loss_weights)
     loss = criterion(cluster_preds, cluster_labels.float().clone())
 
